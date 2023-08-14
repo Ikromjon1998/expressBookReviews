8d5ade90bea9059519330832d613ce1d0dda3ce6
Ochilov <57859849+Ikromjon1998@users.noreply.github.com>
2023-08-14 11:13:04 +0200

    added solutions for task 2&3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,7 +31,18 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const keys = Object.keys(books);
+  const booksByAuthor = keys
+      .filter(
+          key => books[key].author === author
+      ).map(key => books[key]);
+
+  if (booksByAuthor.length > 0) {
+    return res.send(booksByAuthor);
+  }
+
+  return res.status(404).send('No books found by this author');
 });
 
 // Get all books based on title
