bc97c0576cf2e2cb7a765713304bb4a5497ef019
Ochilov <57859849+Ikromjon1998@users.noreply.github.com>
2023-08-14 11:05:58 +0200

    added solution for task 1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,16 +12,22 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+  const book = books[req.params.isbn];
+
+  // Step 3: If the book is found, send it as a response
+  if (book) {
+    return res.send(book);
+  }
+
+  return res.status(404).send('Book not found');
+});
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
